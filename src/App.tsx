160725ebf44cd19b2681e88dbc461e1c6dc2322b
gyeongkoo <rnrudxo2872@naver.com>
2022-11-28 00:49:34 +0900

    simple styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,38 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
+import "./default.css";
 import style from "./App.module.css";
 
+const INIT_NUMBERS = [0, 0, 0, 0, 0];
+
 function App() {
-  const [numbers, setNumbers] = useState<number[]>();
+  const [numbers, setNumbers] = useState<number[]>(INIT_NUMBERS);
+  const isUnSet = useRef<boolean>(true);
 
   const generateNumbers = () => {
+    isUnSet.current = false;
+
     const lottoList = Array.from({ length: 45 }, (_, i) => i + 1);
-    const selectList = Array.from({ length: 5 }, () => 0).map((_, i) => {
-      const randomIdx = Math.floor(Math.random() * 45);
-      const selectIdx = Math.max(0, Math.min(lottoList.length - (1 + i), randomIdx));
+    setNumbers((numbers) =>
+      numbers.map((_, i) => {
+        const randomIdx = Math.floor(Math.random() * 45);
+        const selectIdx = Math.max(0, Math.min(lottoList.length - (1 + i), randomIdx));
 
-      const selectNumber = lottoList[selectIdx] * 1;
-      lottoList.splice(selectIdx, 1);
+        const selectNumber = lottoList[selectIdx] * 1;
+        lottoList.splice(selectIdx, 1);
 
-      return selectNumber;
-    });
-
-    setNumbers(selectList);
+        return selectNumber;
+      })
+    );
   };
 
   return (
     <div className="App">
-      <div>
-        <div className="lotto-circle__wrapper">
+      <div className={style["lotto__app"]}>
+        <div className={style["lotto-circle__wrapper"]}>
           {numbers?.map((item, idx) => (
-            <div key={idx} className="lotto__circle">
-              {item}
+            <div key={idx} className={style["lotto__circle"]}>
+              <span className={style["lotto__number"]}>{isUnSet.current ? null : item}</span>
             </div>
           ))}
         </div>
